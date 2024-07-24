75e4221e0e3cfd2ac414d3edd56992d7a73f85c8
Henrriky <tectutors.123@gmail.com>
2024-07-23 21:49:38 -0300

    feat: create tests e2e to fetch recent questions and create question controller

---
+++ src/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -0,0 +1,66 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+
+describe('Fetch Recent Questions (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[GET] /questions', async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'John doe',
+        email: 'johndoe@example.com',
+        password: await hash('123456', 8),
+      },
+    })
+    const accessToken = jwt.sign({ sub: user.id })
+
+    await prisma.question.createMany({
+      data: [
+        {
+          title: 'Question title',
+          content: 'question content',
+          slug: 'question-title',
+          authorId: user.id,
+        },
+        {
+          title: 'Question title 01',
+          content: 'question content 01',
+          slug: 'question-title-01',
+          authorId: user.id,
+        },
+      ],
+    })
+
+    const response = await request(app.getHttpServer())
+      .get('/questions')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      questions: [
+        expect.objectContaining({ title: 'Question title' }),
+        expect.objectContaining({ title: 'Question title 01' }),
+      ],
+    })
+  })
+})
