f6a1ba9ed818d816181fbdd450d8c5d2f7ffe2d5
Henrriky <tectutors.123@gmail.com>
2024-08-11 00:20:35 -0300

    feat: create database module to provide prisma repositories

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,34 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { PrismaAnswerAttachmentsRepository } from './repositories/prisma-answer-attachments-repository'
+import { PrismaAnswerCommentsRepository } from './repositories/prisma-answer-comments-repository'
+import { PrismaAnswerRepository } from './repositories/prisma-answer-repository'
+import { PrismaQuestionAttachmentsrepository } from './repositories/prisma-question-attachments-repository'
+import { PrismaQuestionCommentsRepository } from './repositories/prisma-question-comments-repository'
+import { PrismaQuestionsRepository } from './repositories/prisma-questions-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerRepository,
+    PrismaQuestionAttachmentsrepository,
+    PrismaQuestionCommentsRepository,
+    {
+      provide: QuestionsRepository,
+      useClass: PrismaQuestionsRepository,
+    },
+  ],
+  exports: [
+    PrismaService,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerRepository,
+    PrismaQuestionAttachmentsrepository,
+    PrismaQuestionCommentsRepository,
+    QuestionsRepository,
+  ],
 })
 export class DatabaseModule {}
