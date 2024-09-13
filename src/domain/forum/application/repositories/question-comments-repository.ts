2892a14146776e0d8161649c58a8e1703e80c91e
Henrriky <tectutors.123@gmail.com>
2024-09-12 23:08:43 -0300

    refactor: transform repositories to abstract class

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,12 +1,13 @@
 import { PaginationParams } from '@/core/repositories/paginations-params'
 import { QuestionComment } from '../../enterprise/entities/Question-Comment'
 
-export interface QuestionCommentsRepository {
-  findById(id: string): Promise<QuestionComment | null>
-  findManyByTopicId(
+export abstract class QuestionCommentsRepository {
+  abstract findById(id: string): Promise<QuestionComment | null>
+  abstract findManyByTopicId(
     questionId: string,
     params: PaginationParams,
   ): Promise<QuestionComment[]>
-  create(questionComment: QuestionComment): Promise<void>
-  delete(questionComment: QuestionComment): Promise<void>
+
+  abstract create(questionComment: QuestionComment): Promise<void>
+  abstract delete(questionComment: QuestionComment): Promise<void>
 }
