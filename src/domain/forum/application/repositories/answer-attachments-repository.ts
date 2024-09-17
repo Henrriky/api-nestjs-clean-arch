580e742964ddef52218e126b28e7cf7e993fb55e
Henrriky <tectutors.123@gmail.com>
2024-09-16 23:20:29 -0300

    feat: update answer attachments repository contract

--- src/domain/forum/application/repositories/answer-attachments-repository.ts
+++ src/domain/forum/application/repositories/answer-attachments-repository.ts
@@ -1,6 +1,9 @@
 import { AnswerAttachment } from '../../enterprise/entities/Answer-Attachment'
 
 export abstract class AnswerAttachmentsRepository {
+  abstract createMany(attachments: AnswerAttachment[]): Promise<void>
+  abstract deleteMany(attachments: AnswerAttachment[]): Promise<void>
+
   abstract findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]>
   abstract deleteManyByAnswerId(answerId: string): Promise<void>
 }
